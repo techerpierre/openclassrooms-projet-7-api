793d9fe10f441aabc3bebeccef21c46f610b1b3e
techernoelpierre <techernoelpierre@gmail.com>
2024-10-04 16:58:29 +0400

    feat: user service

---
+++ src/modules/user/services/user.service.ts
@@ -0,0 +1,24 @@
+import { CreateUser, UpdateUser } from "../dto/user.dto";
+import { User, UserRepository } from "../user.module";
+
+export class UserService {
+    
+    constructor(private readonly userRepository: UserRepository) {}
+
+    async create(data: CreateUser): Promise<User> {
+        return this.userRepository.create(data);
+    }
+
+    async update(id: string, data: UpdateUser): Promise<User> {
+        return this.userRepository.update(id, data);
+    }
+
+    async findOne(id: string, includes: string[]): Promise<User> {
+        return this.userRepository.findOne(id, includes);
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        return this.userRepository.findByEmail(email);
+    }
+
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,2 +1,3 @@
 export { User } from "./entities/user.entity";
 export { UserRepository } from "./repository/user.repository";
+export { UserService } from "./services/user.service";
